28c5da4dec584b9c107434617dd31f4056151d58
coderElizabeth <107820121+coderElizabeth@users.noreply.github.com>
2022-07-18 14:31:37 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+function validateForm() {
+    let x = document.forms["myForm"]["n1"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["fav_language"].value;
+    if (x == "") {
+      alert("Trip type must be sellected out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["f"].value;
+    if (x == "") {
+      alert("location must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["t"].value;
+    if (x == "") {
+      alert("location must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["date"].value;
+    if (x == "") {
+      alert("Date must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["1"].value;
+    if (x == "") {
+      alert("Gender must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["a1"].value;
+    if (x == "") {
+      alert("Age must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["n2"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["2"].value;
+    if (x == "") {
+      alert("Gender must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["A2"].value;
+    if (x == "") {
+      alert("Age must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["n3"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["3"].value;
+    if (x == "") {
+      alert("Gender must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["a3"].value;
+    if (x == "") {
+      alert("Age must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Full name must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["email"].value;
+    if (x == "") {
+      alert("Email must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    let x = document.forms["myForm"]["a"].value;
+    if (x == "") {
+      alert("Number must be filled out");
+      return false;
+    }
+  }
